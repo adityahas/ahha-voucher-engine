8dcab0481f69e287d6742340055e862bbdb456bd
Aditya Hastungkoro Hadi <aditya.hadi@akarinti.tech>
2024-12-18 16:49:50 +0700

    feat: Refactor user module, add base entity, and update database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { VoucherModule } from './voucher/voucher.module';
+import { DatabaseModule } from './database/database.module';
+import { QuestModule } from './quest/quest.module';
+import { BaseModule } from './base/base.module';
+import { AdminUserModule } from './admin-user/admin-user.module';
 
 @Module({
-  imports: [UserModule, VoucherModule],
+  imports: [
+    UserModule,
+    VoucherModule,
+    DatabaseModule,
+    QuestModule,
+    BaseModule,
+    AdminUserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from '../../base/entities/base.entity';
+
+@Entity('users')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @Column({ type: 'boolean', default: false })
+  is_active: boolean;
+
+  @Column({ type: 'boolean', default: false })
+  is_deleted: boolean;
+}
