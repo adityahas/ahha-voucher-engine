d4305d768852cbdbc3b1f58e3362d0d5a364bdb6
Aditya Hastungkoro Hadi <aditya.hadi@akarinti.tech>
2024-12-19 11:05:38 +0700

    feat: Integrate dotenv for environment variable management and update database configuration for multiple modules

--- src/admin-user/admin-user.module.ts
+++ src/admin-user/admin-user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminUserService } from './admin-user.service';
 import { AdminUserController } from './admin-user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminUser } from './entities/admin-user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AdminUser])],
   controllers: [AdminUserController],
   providers: [AdminUserService],
 })
