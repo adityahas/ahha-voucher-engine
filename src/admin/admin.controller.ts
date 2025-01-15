53d779ced6c4c9492fc03a49ccc634bd3858f6b8
Aditya Hastungkoro Hadi <adityahas@gmail.com>
2025-01-15 16:44:20 +0700

    Integrate user auth with JWT token with custom AuthGuard

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -7,7 +7,9 @@ import {
   Patch,
   Post,
   Req,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
@@ -25,26 +27,31 @@ export class AdminController extends BaseController {
     return this.adminService.login(this.getDatabaseName(req), loginAdminDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() createAdminDto: CreateAdminDto) {
     return this.adminService.create(createAdminDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   findAll() {
     return this.adminService.findAll();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.adminService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
     return this.adminService.update(+id, updateAdminDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.adminService.remove(+id);
--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -5,9 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin } from './entities/admin.entity';
 import { ClientsService } from '../client/client.service';
 import { Client } from '../client/entities/client.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Admin, Client])],
+  imports: [
+    TypeOrmModule.forFeature([Admin, Client]),
+    JwtModule.register({
+      secret: 'yourSecretKey',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AdminController],
   providers: [AdminService, ClientsService],
 })
