7dc165fb40fe835b20247629885a9ae1fb0a1038
Aditya Hastungkoro Hadi <aditya.hadi@akarinti.tech>
2025-06-27 10:54:06 +0700

    feat: add role field to User entity
    - Add `role` column to the `User` entity with default value `Role.USER`.
    - Import Role enum from ACL module.
    - This change requires a database migration.

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { BaseEntity } from '../../../base/entities/base.entity';
+import { Role } from '../../../acl/roles.enum';
 
 @Entity('users')
 export class User extends BaseEntity {
@@ -15,9 +16,17 @@ export class User extends BaseEntity {
   @Column({ type: 'varchar' })
   password: string;
 
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.USER,
+  })
+  role: Role;
+
   @Column({ type: 'boolean', default: false })
   is_active: boolean;
 
   @Column({ type: 'boolean', default: false })
   is_deleted: boolean;
 }
+
