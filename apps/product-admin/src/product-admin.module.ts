f55857a3cc69cc6d78e13e56f5a2d1259d3da880
Aditya Hastungkoro Hadi <aditya.hadi@akarinti.tech>
2026-04-18 14:20:41 +0700

    feat: Integrate authentication and access control into Product Admin module
    - Added guards (`AdminJwtGuard`, `AclGuard`) and `Permissions` decorator to secure Product Admin endpoints.
    - Included authentication and database modules in Product Admin module with dynamic TypeORM configuration.
    - Applied middleware (`SubdomainMiddleware`, `CredentialMiddleware`) to all routes for enhanced security.
    - Updated Nest CLI configuration for `product-admin` entry file and TypeScript configuration path.
    - Extended run configurations to include `product-admin` and `product-consumer` services.

--- apps/product-admin/src/product-admin.module.ts
+++ apps/product-admin/src/product-admin.module.ts
@@ -1,13 +1,41 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from '@core/product/entities/product.entity';
 import { ProductAdminService } from './product-admin.service';
 import { ProductAdminController } from './product-admin.controller';
+import { DatabaseModule } from '@core/database';
+import { AuthModule } from '@core/auth';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { CredentialMiddleware, SubdomainMiddleware } from '@core/middleware';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      namingStrategy: new SnakeNamingStrategy(),
+      synchronize: process.env.DB_SYNC == 'true',
+      logging: process.env.DB_LOGGING != 'false',
+      autoLoadEntities: true,
+      entities: [__dirname + '/../../../**/*.entity{.ts,.js}'],
+    }),
+    TypeOrmModule.forFeature([ProductEntity]),
+    DatabaseModule,
+    AuthModule,
+  ],
   providers: [ProductAdminService],
   controllers: [ProductAdminController],
   exports: [ProductAdminService],
 })
-export class ProductAdminModule {}
+export class ProductAdminModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(SubdomainMiddleware, CredentialMiddleware).forRoutes('*');
+  }
+}
--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,2 +1,7 @@
 export * from './auth.module';
 export * from './acl.service';
+export * from './guards/admin-jwt.guard';
+export * from './guards/consumer-jwt.guard';
+export * from './guards/acl.guard';
+export * from './decorators/permissions.decorator';
+export * from './roles.enum';
