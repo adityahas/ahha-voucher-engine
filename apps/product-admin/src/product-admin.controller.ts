aaaf9bdde6e8174f56cda630da291168fa54053e
Aditya Hastungkoro Hadi <aditya.hadi@akarinti.tech>
2026-04-18 14:20:41 +0700

    feat: Integrate authentication and access control into Product Admin module
    - Added guards (`AdminJwtGuard`, `AclGuard`) and `Permissions` decorator to secure Product Admin endpoints.
    - Included authentication and database modules in Product Admin module with dynamic TypeORM configuration.
    - Applied middleware (`SubdomainMiddleware`, `CredentialMiddleware`) to all routes for enhanced security.
    - Updated Nest CLI configuration for `product-admin` entry file and TypeScript configuration path.
    - Extended run configurations to include `product-admin` and `product-consumer` services.

--- apps/product-admin/src/product-admin.controller.ts
+++ apps/product-admin/src/product-admin.controller.ts
@@ -6,36 +6,44 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductAdminService } from './product-admin.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { AdminJwtGuard, AclGuard, Permissions } from '@core/auth';
 
 @Controller('redistro/products')
+@UseGuards(AdminJwtGuard, AclGuard)
 export class ProductAdminController {
   constructor(private readonly productService: ProductAdminService) {}
 
   @Post()
+  @Permissions('write:products')
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
   @Get()
+  @Permissions('read:products')
   findAll() {
     return this.productService.findAll();
   }
 
   @Get(':id')
+  @Permissions('read:products')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(id);
   }
 
   @Patch(':id')
+  @Permissions('write:products')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @Permissions('write:products')
   remove(@Param('id') id: string) {
     return this.productService.remove(id);
   }
